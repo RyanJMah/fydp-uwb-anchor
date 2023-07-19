0c148f13b499689c8cd8bcd1b76980f0fd66c195
Ryan Mah <rmah@uwaterloo.ca>
2023-07-19 18:18:56 -0400

    done

--- Src/MQTT/app_mqtt.h
+++ Src/MQTT/app_mqtt.h
@@ -22,35 +22,9 @@
 ************************************************************/
 typedef struct
 {
-    uint8_t rssi;
     uint8_t status;
-
     uint32_t timestamp;
-
     int32_t distance_mm;
-
-    /*
-     * The angles are represented as a 16-bit fixed-point number
-     * with 0 integer bits and 16 fractional bits (Q0.16).
-     *
-     * The angles are also represented as "how many multiples
-     * of 2pi" the angle is. So for example, 0.5 corresponds to
-     * 0.5*2pi = pi = 180 degrees.
-     *
-     * Thus, the angles in this struct can be converted to
-     * degrees like so:
-     *
-     * float convert_aoa_2pi_q16_to_deg(int16_t aoa_2pi_q16)
-     * {
-     *     return (360.0 * aoa_2pi_q16 / (1 << 16));
-     * }
-     */
-
-    int16_t iphone_aoa_2pi_q16;
-    uint8_t iphone_aoa_fom;         // fom: figure-of-merit
-
-    int16_t anchor_aoa_2pi_q16;
-    uint8_t anchor_aoa_fom;
 } TelemetryData_t;
 
 /*************************************************************
@@ -60,14 +34,9 @@ ALWAYS_INLINE MqttRetCode_t TelemetryData_Publish(struct ranging_measurements* r
 {
     TelemetryData_t telem_data =
     {
-        .rssi               = rm->rssi,
         .status             = rm->status,
         .timestamp          = osKernelSysTick(),
         .distance_mm        = rm->distance_mm,
-        .iphone_aoa_2pi_q16 = rm->remote_aoa_azimuth_2pi,
-        .iphone_aoa_fom     = rm->remote_aoa_azimuth_fom,
-        .anchor_aoa_2pi_q16 = rm->local_aoa_measurements[0].aoa_2pi,
-        .anchor_aoa_fom     = rm->local_aoa_measurements[0].aoa_fom
     };
 
     return MqttClient_Publish( DATA_TOPIC,
