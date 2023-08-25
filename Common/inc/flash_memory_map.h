103864b0101ff1cf0b85f12a0ae6e090832fdcc4
Ryan Mah <rmah@uwaterloo.ca>
2023-08-24 21:35:58 -0600

    add common folder, for code common to bootloader and app

---
+++ Common/inc/flash_memory_map.h
@@ -0,0 +1,90 @@
+#pragma once
+
+#include "macros.h"
+
+#define MCU_FLASH_SIZE      ( 512 * 1024 )  // 512KB
+#define FLASH_PAGE_SIZE     ( 4096 )
+
+/*
+ * 
+ *  Memory map of ALL flash in MCU looks like this:
+ *
+ *  --------------------------------
+ *  |                              |
+ *  |          Bootloader          |    (0x0000 0000 - 0x0000 2800)     10 kB
+ *  |          (10 pages)          |
+ *  |                              |
+ *  --------------------------------
+ *  |                              |
+ *  |   Application Data Storage   |    (0x0000 2800 - 0x0000 3000)     2 kB
+ *  |         (2 pages)            |
+ *  |                              |
+ *  --------------------------------
+ *  |                              |
+ *  |         Application          |    (0x0000 3000 - 0x0007 FFFF)     500 kB
+ *  |         (125 pages)          |
+ *  |                              |
+ *  --------------------------------                                   ---------
+ *                                                               Total  500 kB
+ */
+
+#define FLASH_BOOTLOADER_SIZE           ( 10 * FLASH_PAGE_SIZE )
+#define FLASH_APP_DATA_SIZE             ( 2 * FLASH_PAGE_SIZE )
+#define FLASH_APP_SIZE                  ( MCU_FLASH_SIZE - FLASH_BOOTLOADER_SIZE - FLASH_APP_DATA_SIZE )
+
+#define FLASH_BOOTLOADER_START_ADDR     ( 0x00000000 )
+#define FLASH_BOOTLOADER_END_ADDR       ( FLASH_BOOTLOADER_START_ADDR + FLASH_BOOTLOADER_SIZE - 1 )
+
+#define FLASH_APP_DATA_START_ADDR       ( FLASH_BOOTLOADER_END_ADDR + 1 )
+#define FLASH_APP_DATA_END_ADDR         ( FLASH_APP_DATA_START_ADDR + FLASH_APP_DATA_SIZE - 1 )
+
+#define FLASH_APP_START_ADDR            ( FLASH_APP_DATA_END_ADDR + 1 )
+#define FLASH_APP_END_ADDR              ( FLASH_APP_START_ADDR + FLASH_APP_SIZE - 1 )
+
+// Check if sizes are multiples of page size
+
+#if ( FLASH_BOOTLOADER_SIZE % FLASH_PAGE_SIZE ) != 0
+#error "FLASH_BOOTLOADER_SIZE is not a multiple of FLASH_PAGE_SIZE"
+#endif
+
+
+#if ( FLASH_APP_DATA_SIZE % FLASH_PAGE_SIZE ) != 0
+#error "FLASH_APP_DATA_SIZE is not a multiple of FLASH_PAGE_SIZE"
+#endif
+
+#if ( FLASH_APP_SIZE % FLASH_PAGE_SIZE ) != 0
+#error "FLASH_APP_SIZE is not a multiple of FLASH_PAGE_SIZE"
+#endif
+
+// Make sure that we are using all of the flash
+#if ( FLASH_BOOTLOADER_SIZE + FLASH_APP_DATA_SIZE + FLASH_APP_SIZE ) != MCU_FLASH_SIZE
+#error "wasted flash space in memory map"
+#endif
+
+// Make sure that the bootloader and app data are not overlapping
+#if !( FLASH_BOOTLOADER_END_ADDR < FLASH_APP_DATA_START_ADDR )
+#error "FLASH_BOOTLOADER_END_ADDR overlaps FLASH_APP_DATA_START_ADDR"
+#endif
+
+// Make sure that the app data and app are not overlapping
+#if !( FLASH_APP_DATA_END_ADDR < FLASH_APP_START_ADDR )
+#error "FLASH_APP_DATA_END_ADDR overlaps FLASH_APP_START_ADDR"
+#endif
+
+// Make sure that the app is not overlapping the bootloader
+#if !( FLASH_APP_START_ADDR > FLASH_BOOTLOADER_END_ADDR )
+#error "FLASH_APP_START_ADDR overlaps FLASH_BOOTLOADER_END_ADDR"
+#endif
+
+// Make sure that the app is not overlapping the app data
+#if !( FLASH_APP_START_ADDR > FLASH_APP_DATA_END_ADDR )
+#error "FLASH_APP_START_ADDR overlaps FLASH_APP_DATA_END_ADDR"
+#endif
+
+// Make sure that the app is not overlapping the end of flash
+#if !( FLASH_APP_END_ADDR < MCU_FLASH_SIZE )
+#error "FLASH_APP_END_ADDR overlaps MCU_FLASH_SIZE"
+#endif
+
+
+
